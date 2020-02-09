8d5b3deab828ecd55bd0946d868611702eb917e1
Akash Gvalani <akash_gvalani@ibi.com>
2020-02-08 20:59:13 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    // Leet code problem Spiral Matrix Java
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int [][] m = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(s.spiralOrder(m));
+    }
+}
